03155c2265865718cddac2601052655d072326fd
Dipanshu <dipanshuyadav9385@gmail.com>
2023-12-22 15:11:23 +0530

    created react-app using vite and installed tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function App() {
+  
+
+  return (
+    <h1 className='text-4xl font-semibold text-center text-blue-800 mt-10'>Hello World</h1>
+  )
+}
+
+export default App
