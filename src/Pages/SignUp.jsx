0e0d8dc610c749e1921d7f76c18c8455c003bb33
Dipanshu <dipanshuyadav9385@gmail.com>
2023-12-28 11:34:04 +0530

    completed SignUp UI

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>
-      <h1 className='text-3xl text-white text-center font-semibold'>SignUp</h1>
+    <div className='max-w-sm mx-auto'>
+      <h1 className='text-3xl text-slate-900 text-center font-bold my-9'>SignUp</h1>
+        <form action="/signup" method='post' className='p-16 bg-slate-300 shadow-2xl rounded-lg'>
+      <div className='flex flex-col gap-5'>
+          <input type="text" name="" id="" placeholder='Username' className='py-1 px-3 outline-none rounded-md' />
+          <input type="email" name="" id="" placeholder='email' className='py-1 px-3 outline-none rounded-md' />
+          <input type="password" name="" id="" placeholder='password' className='py-1 px-3 outline-none rounded-md' />
+          <input type="submit" value="Sign Up" className='bg-orange-600 py-2 text-white rounded-md hover:opacity-80'/>
+          <input type="submit" value="Sign Up with Google" className='bg-blue-600 py-2 text-white rounded-md hover:opacity-80'/>
+          <a href="/sign_in">already have an Account ? <strong>Sign In</strong></a>
+      </div>
+        </form>
+
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,18 @@ import { FaSearch } from 'react-icons/fa';
 import { Link, NavLink } from 'react-router-dom';
 const Header = () => {
   return (
-    <header className=' w-full bg-gray-400 shadow-xl py-4 '>
+    <header className=' w-full bg-slate-300 shadow-xl py-4 '>
       <div className=' flex flex-wrap flex-row justify-between items-center gap-4 px-4 max-w-6xl mx-auto'>
         <Link to="/">
-        <h1 className=' text-sm sm:text-xl font-semibold'>
-          <span className='text-red-600'>Royal</span>
-          <span className='text-slate-700'>Estate</span>
-        </h1>
+          <h1 className=' text-sm sm:text-xl font-semibold'>
+            <span className='text-red-600'>Royal</span>
+            <span className='text-slate-700'>Estate</span>
+          </h1>
         </Link>
 
         <form className='bg-zinc-100 py-1 px-5 rounded-lg flex justify-between items-center w-58  sm:w-72'>
           <input type="text" placeholder='Search...' className='bg-transparent outline-none' />
-          <FaSearch className=''/>
+          <FaSearch className='' />
         </form>
 
         <nav>
@@ -22,23 +22,26 @@ const Header = () => {
 
             <li className='hidden sm:inline'>
               <NavLink to="/"
-                className={({ isActive }) => ` duration-200 ${isActive ? 'text-orange-500' : 'text-slate-100'}`}>
+                className={({ isActive }) => ` duration-200 ${isActive ? 'text-orange-500' : 'text-slate-900'}`}>
                 Home
               </NavLink>
             </li>
             <li className='hidden sm:inline'>
               <NavLink to="/about"
-                className={({isActive}) => ` duration-200 ${isActive ? 'text-orange-500' : 'text-slate-100'}`}>
+                className={({ isActive }) => ` duration-200 ${isActive ? 'text-orange-500' : 'text-slate-900'}`}>
                 About
               </NavLink>
             </li>
-
-
-
             <li>
               <NavLink to="/sign_in"
-                className={({ isActive }) => `duration-200 ${isActive ? 'text-orange-500' : 'text-slate-100'}`}>
+                className={({ isActive }) => `duration-200 ${isActive ? 'text-orange-500' : 'text-slate-900'}`}>
                 Sign In
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/sign_up"
+                className={({ isActive }) => `duration-200 ${isActive ? 'text-orange-500' : 'text-slate-900'}`}>
+                Sign Up
               </NavLink>
             </li>
 
